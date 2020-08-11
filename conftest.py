2b91f471f1379a075fc829b9a9a54153b4acf12b
AutomatedOwl <automatedowl@autobox.net>
2020-08-11 13:36:23 +0300

    added pypath option

--- conftest.py
+++ conftest.py
@@ -3,3 +3,4 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption('--test-dir', action='store')
     parser.addoption('--interface', action='store')
+    parser.addoption("--pypath", dest="pypath", action="append")
